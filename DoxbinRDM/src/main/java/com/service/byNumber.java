43982a6fb95c94dd51dbcfdb436fa0812a50192f
Rassul231 <153492109+Rassul231@users.noreply.github.com>
2024-02-16 13:31:08 +0600

    Update byNumber.java

--- DoxbinRDM/src/main/java/com/service/byNumber.java
+++ DoxbinRDM/src/main/java/com/service/byNumber.java
@@ -6,14 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 public class byNumber {
+
     ArrayList<Peoples> peoples = new ArrayList<>();
 
     protected void Find(int n) {
         String sql = "SELECT id,name, surname, gender, phone, \"Salary\", \"Occupation\", \"University\", \"Gpa\", \"Education\", \"Married\",\"Address\" FROM public.\"Peoples\" WHERE phone = ?;";
         try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/edu_reserve_db", "postgres", "2006");
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1,n);
-            ResultSet rs = pstmt.executeQuery();
+             pstmt.setLong(1,n);
+             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
@@ -21,7 +22,7 @@ public class byNumber {
                 boolean gender = rs.getBoolean("gender");
                 Float gpa = rs.getFloat("Gpa");
                 String University = rs.getString("University");
-                int phone = rs.getInt("phone");
+                long phone = rs.getLong("phone");
                 String Occupation = rs.getString("Occupation");
                 int Salary = rs.getInt("Salary");
                 String Education = rs.getString("Education");
@@ -29,15 +30,14 @@ public class byNumber {
                 String Address = rs.getString("Address");
                 Peoples Peoples;
                 if (University != null) {
-                    System.out.println(conn);
-                    Peoples = new Student(id, name, surname, gender, gpa, University, phone,Address);
-                    peoples.add(Peoples);
+                        Peoples = new Student(id, name, surname, gender, gpa, University, phone,Address);
+                        peoples.add(Peoples);
                 } else if (Occupation != null) {
-                    Peoples = new Worker(id, name, surname, gender, phone, Salary, Occupation,Education,Married,Address);
-                    peoples.add(Peoples);
+                        Peoples = new Worker(id, name, surname, gender, phone, Salary, Occupation,Education,Married,Address);
+                        peoples.add(Peoples);
                 } else {
-                    Peoples = new Peoples(id, name, surname, gender, phone,Address);
-                    peoples.add(Peoples);
+                        Peoples = new Peoples(id, name, surname, gender, phone,Address);
+                        peoples.add(Peoples);
                 }
                 conn.close();
 
@@ -51,22 +51,22 @@ public class byNumber {
 
     }
 
-    protected void Add(String Name,String Surname,boolean Gender,int Phone,int Salary,String Occupation,String University,Float Gpa,String Education,Boolean Married,String Address) {
+    protected void Add(String Name,String Surname,boolean Gender,long Phone,int Salary,String Occupation,String University,Float Gpa,String Education,Boolean Married,String Address) {
         String sql = "INSERT INTO public.\"Peoples\" (name, surname, gender, phone, \"Salary\", \"Occupation\", \"University\", \"Gpa\",\"Education\",\"Married\",\"Address\") VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?,?);";
         try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/edu_reserve_db", "postgres", "2006");
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, Name);
-            pstmt.setString(2, Surname);
-            pstmt.setBoolean(3, Gender);
-            pstmt.setInt(4, Phone);
-            pstmt.setInt(5, Salary);
-            pstmt.setString(6, Occupation);
-            pstmt.setString(7, University);
-            pstmt.setFloat(8, Gpa);
-            pstmt.setString(9,Education);
-            pstmt.setBoolean(10,Married);
-            pstmt.setString(11,Address);
-            pstmt.executeUpdate();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, Name);
+                stmt.setString(2, Surname);
+                stmt.setBoolean(3, Gender);
+                stmt.setLong(4, Phone);
+                stmt.setInt(5, Salary);
+                stmt.setString(6, Occupation);
+                stmt.setString(7, University);
+                stmt.setFloat(8, Gpa);
+                stmt.setString(9,Education);
+                stmt.setBoolean(10,Married);
+                stmt.setString(11,Address);
+                stmt.executeUpdate();
         }
         catch (SQLException e) {
             System.out.println("Connection error: " + e.getMessage());
